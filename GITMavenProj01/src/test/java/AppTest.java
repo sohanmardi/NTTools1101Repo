46ad7311c2007ec3a12e094b83eaa328cf0b02bf
Sohan Mardi <Sohan Mardi@LAPTOP-POJKBD69>
2024-07-21 11:39:56 +0530

    adding more tests

--- GITMavenProj01/src/test/java/AppTest.java
+++ GITMavenProj01/src/test/java/AppTest.java
@@ -1,4 +1,4 @@
-package com.nt.test;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -15,4 +15,19 @@ public class AppTest {
 			assertEquals(expected,actual);
 		}
 		
+	@Test
+	public void testwithNegatives() {
+		Arithmetic ar=new Arithmetic();
+		int expected =-40;
+		int actual=ar.sum(-20, -20);
+		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void testwithZeros() {
+		Arithmetic ar=new Arithmetic();
+		int expected =0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected,actual);
+	}
 }
